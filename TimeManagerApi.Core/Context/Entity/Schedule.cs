b6e79c2fdb814dce98fbce7e9f13726e53935171
dkataev <45999287+HappyFox404@users.noreply.github.com>
2023-03-30 17:44:07 +0300

    Timestamp
    Add TimeStamp count in model

--- TimeManagerApi.Core/Context/Entity/Schedule.cs
+++ TimeManagerApi.Core/Context/Entity/Schedule.cs
@@ -6,6 +6,13 @@ public class Schedule : BaseEntity
 {
     public DateTime Day { get; set; }
     public Guid UserId { get; set; }
+    public int TimeStampCount
+    {
+        get
+        {
+            return TimeStamps.Count();
+        }
+    }
     [JsonIgnore] public virtual User User { get; set; } = null!;
     [JsonIgnore] public virtual ICollection<TimeStamp> TimeStamps { get; set; } = new List<TimeStamp>();
 }
